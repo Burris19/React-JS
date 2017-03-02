aaf89456aaa587f254d93f9382b4209c9ced98e6
Burris19 <julianhernandez19@gmail.com>
2017-03-02 07:39:34 -0600

    [update] modify component

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,21 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-
 module.exports = {
   context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./app/dist/index.js",
-  modules: {
+  entry: "./app/dist/index.jsx",
+  output: {
+    path: __dirname + "/app/js",
+    filename: "index.min.js"
+  },  
+  module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugin: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query:{
+          presets: ['react', 'es2015'],
+          // plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
         }
       }
     ]
-  },
-  output: {
-    path: __dirname + "/app/js",
-    filename: "scripts.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  }
 };
